4a30349b2e6fc775fc097cc4e0f85b774c29b1ab
Bull101 <43958984+Bull101@users.noreply.github.com>
2018-10-08 22:50:45 +0200

    Reorga

---
+++ STM32 Test DRV1835/STM32 Test DRV1835/src/main.cpp
@@ -0,0 +1,88 @@
+#include "mbed.h"
+
+DigitalOut Enable_(D3);
+DigitalOut M0(D4);
+DigitalOut M1(D5);
+DigitalOut M2(D6);
+DigitalOut STEP(D7);
+DigitalOut Dir(D8);
+DigitalIn btn(D12);
+DigitalOut LED(LED3);
+int MaxTurn = 800;
+int StepDelay = 1000;
+void StepMode(int mode);
+
+int main()
+{
+    int i, j;
+
+    while (1)
+    {
+        if(btn == 1)
+        {
+        int mode = 1;
+        Enable_=1;
+        for (i = 0; i <= 8; i++)
+        {
+            LED=1;
+            Dir = 0;
+            for (j = 0; j <= MaxTurn; j++)
+            {
+                STEP=1;
+                wait_us(StepDelay);
+                STEP=0;
+            }
+            Dir = 1;
+            for (j = 0; j <= MaxTurn; j++)
+            {
+                STEP=1;
+                wait_us(StepDelay);
+                STEP=0;
+            }
+            mode = mode * 2;
+            LED=0;
+            wait(3);
+        }
+        Enable_=0;
+        LED=0;
+        }
+
+    }
+}
+
+void Set_StepMode(int mode)
+{
+    switch (mode)
+    {
+    case 1:
+        M0 = 0;
+        M1 = 0;
+        M2 = 0;
+        break;
+    case 2:
+        M0 = 1;
+        M1 = 0;
+        M2 = 0;
+        break;
+    case 4:
+        M0 = 0;
+        M1 = 1;
+        M2 = 0;
+        break;
+    case 8:
+        M0 = 1;
+        M1 = 1;
+        M2 = 0;
+        break;
+    case 16:
+        M0 = 0;
+        M1 = 0;
+        M2 = 1;
+        break;
+    case 32:
+        M0 = 1;
+        M1 = 0;
+        M2 = 1;
+        break;
+    }
+}
